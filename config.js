82f4ca4c8e62a365c1a9a3e138146389852772af
diesel25092007 <95993965+diesel25092007@users.noreply.github.com>
2022-09-27 19:41:47 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBV1MOaTftAJz-AyZ-WPJ1D3ore9zVO9VU",
+  authDomain: "capstone-1-2f2ea.firebaseapp.com",
+  projectId: "capstone-1-2f2ea",
+  storageBucket: "capstone-1-2f2ea.appspot.com",
+  messagingSenderId: "629328252779",
+  appId: "1:629328252779:web:3bbb2fcf3817533498c62e"
+};
